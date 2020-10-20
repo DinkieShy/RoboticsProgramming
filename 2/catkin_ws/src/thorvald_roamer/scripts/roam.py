71986a4e34566021d5b0e903596562958cbc3b98
Dinkie <david.churchill72@gmail.com>
2020-10-20 16:45:08 +0100

    more comments

--- 2/catkin_ws/src/thorvald_roamer/scripts/roam.py
+++ 2/catkin_ws/src/thorvald_roamer/scripts/roam.py
@@ -6,6 +6,10 @@ import math
 import time
 
 class RoamerMover:
+    """
+    Simple class to move based on messages publishes to /thorvald_001/clear
+    "R" indicates "clear to the right", "F" -> "clear to the front" etc.
+    """
     def __init__(self):
         self.sub = rospy.Subscriber("/thorvald_001/clear", String, callback=self.move)
         self.pub = rospy.Publisher("/thorvald_001/twist_mux/cmd_vel", Twist)
@@ -34,29 +38,32 @@ class RoamerMover:
         self.pub.publish(twist)
 
 class RoamerWatcher:
+    """
+    Publishes the aforementioned messages to the /thorvald_001/clear topic
+    """
     def __init__(self):
         self.sub = rospy.Subscriber("/thorvald_001/scan", LaserScan, callback=self.watch)
         self.pub = rospy.Publisher("/thorvald_001/clear", String)
         self.minRange = 2.5
 
-    def checkAreaClear(self, sweep, start, stop):
+    def checkAreaClear(self, sweep, start, stop): # Returns false if any value between start and stop are below self.minRange, else true
         for i in range(start, stop):
             if sweep[i] < self.minRange:
                 return False
         return True
 
-    def watch(self, msg): # Watch for objects, call RoamerMover with clear/notclear
+    def watch(self, msg):
         sweep = msg.ranges
-        stringToPublish = ""
         # sweep[0] is rightmost, sweep[len] is leftmost
+        stringToPublish = ""
 
-        if self.checkAreaClear(sweep, 0, len(sweep)/5):
+        if self.checkAreaClear(sweep, 0, len(sweep)/5): # Check first 5th of sweep
             stringToPublish += "R"
 
-        if self.checkAreaClear(sweep, (4/5)*len(sweep), len(sweep)):
+        if self.checkAreaClear(sweep, (4/5)*len(sweep), len(sweep)): # Check last 5th of sweep
             stringToPublish += "L"
 
-        if self.checkAreaClear(sweep, len(sweep)/5, (4/5)*len(sweep)):
+        if self.checkAreaClear(sweep, len(sweep)/5, (4/5)*len(sweep)): # Check centre 3/5ths of sweep
             stringToPublish += "F"
 
         self.pub.publish(stringToPublish)
@@ -66,3 +73,5 @@ mover = RoamerMover()
 watcher = RoamerWatcher()
 
 rospy.spin()
+
+# Mover is based on Watcher, and Watcher is based on messages from /thorvald_001/scan, so no while loop needed
