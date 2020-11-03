8ecf0194c39f0fe20c3098254d846e45601d5d6f
Dinkie <david.churchill72@gmail.com>
2020-11-03 16:23:42 +0000

    stop when see green, also that launch cmd I forgot

--- 2/catkin_ws/src/thorvald_roamer/scripts/roam.py
+++ 2/catkin_ws/src/thorvald_roamer/scripts/roam.py
@@ -3,6 +3,7 @@ from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
 from geometry_msgs.msg import PoseStamped
 from std_msgs.msg import String
+from std_msgs.msg import Bool
 import tf
 import math
 import sys
@@ -19,32 +20,38 @@ class RoamerMover:
         self.closestPointPub = rospy.Publisher("/thorvald_" + id + "/closestPoint", PoseStamped, queue_size=0)
         self.forwardRate = (2)
         self.turnRate = (math.pi/2)/2
+        self.stopSub = rospy.Subscriber("/thorvald_" + id + "/STOP", Bool, self.stopCB)
+        self.stop = False
+
+    def stopCB(self, msg):
+        self.stop = msg.data
 
     def watch(self, msg):
-        sweep = msg.ranges
-        # sweep[0] is rightmost, sweep[len] is leftmost
-        stringToPublish = ""
+        if not self.stop:
+            sweep = msg.ranges
+            # sweep[0] is rightmost, sweep[len] is leftmost
+            stringToPublish = ""
 
-        if self.checkAreaClear(sweep, 0, (len(sweep))/6): # Check first 6th of sweep
-            stringToPublish += "R"
+            if self.checkAreaClear(sweep, 0, (len(sweep))/6): # Check first 6th of sweep
+                stringToPublish += "R"
 
-        if self.checkAreaClear(sweep, (2*len(sweep))/6, (4*len(sweep))/6): # Check centre 1/2th of sweep
-            stringToPublish += "F"
+            if self.checkAreaClear(sweep, (2*len(sweep))/6, (4*len(sweep))/6): # Check centre 1/2th of sweep
+                stringToPublish += "F"
 
-        if self.checkAreaClear(sweep, (5*len(sweep))/6, len(sweep)): # Check last 6th of sweep
-            stringToPublish += "L"
+            if self.checkAreaClear(sweep, (5*len(sweep))/6, len(sweep)): # Check last 6th of sweep
+                stringToPublish += "L"
 
-        self.move(stringToPublish)
+            self.move(stringToPublish)
 
-        minIndex = sweep.index(min(sweep)) # Find minimum value in array of distances, then get the index of that distance in the array
-        angle2D = minIndex*msg.angle_increment + msg.angle_min # Multiply by ancle_increment parameter from scanner, and fix offset (msg.angle_min in this case is -pi/2, thanks Marc)
+            minIndex = sweep.index(min(sweep)) # Find minimum value in array of distances, then get the index of that distance in the array
+            angle2D = minIndex*msg.angle_increment + msg.angle_min # Multiply by ancle_increment parameter from scanner, and fix offset (msg.angle_min in this case is -pi/2, thanks Marc)
 
-        angleTF = PoseStamped()
-        angleTF.header = msg.header # Duplicate message header for time/frame_id from scanner
-        angleTF.pose.orientation.z = math.sin(angle2D/2) # Convert from 2D angle to Quaternion (as done in Marc's lecture 3)
-        angleTF.pose.orientation.w = math.cos(angle2D/2)
-        
-        self.closestPointPub.publish(angleTF) # Publish to closestPoint topic
+            angleTF = PoseStamped()
+            angleTF.header = msg.header # Duplicate message header for time/frame_id from scanner
+            angleTF.pose.orientation.z = math.sin(angle2D/2) # Convert from 2D angle to Quaternion (as done in Marc's lecture 3)
+            angleTF.pose.orientation.w = math.cos(angle2D/2)
+            
+            self.closestPointPub.publish(angleTF) # Publish to closestPoint topic
 
     def checkAreaClear(self, sweep, start, stop): # Returns false if any value between start and stop are below self.minRange, else true
         for i in range(start, stop):
@@ -53,26 +60,27 @@ class RoamerMover:
         return True
 
     def move(self, direction):
-        forwardSpeed = 0
-        turnSpeed = 0
+        if not self.stop:
+            forwardSpeed = 0
+            turnSpeed = 0
 
-        if "F" in direction: # clear ahead
-            forwardSpeed = self.forwardRate
-        if "L" in direction: # clear left
-            turnSpeed += self.turnRate
-        if "R" in direction: # clear right
-            turnSpeed -= self.turnRate
+            if "F" in direction: # clear ahead
+                forwardSpeed = self.forwardRate
+            if "L" in direction: # clear left
+                turnSpeed += self.turnRate
+            if "R" in direction: # clear right
+                turnSpeed -= self.turnRate
 
-        #if clear both left and right, turnspeed == 0 but forwardspeed == 2
+            #if clear both left and right, turnspeed == 0 but forwardspeed == 2
 
-        if turnSpeed == 0 and forwardSpeed == 0: # If not clear in any direction, turn on the spot
-            turnSpeed += self.turnRate
+            if turnSpeed == 0 and forwardSpeed == 0: # If not clear in any direction, turn on the spot
+                turnSpeed += self.turnRate
 
-        twist = Twist()
-        twist.linear.x = forwardSpeed
-        twist.angular.z = turnSpeed
+            twist = Twist()
+            twist.linear.x = forwardSpeed
+            twist.angular.z = turnSpeed
 
-        self.twistPub.publish(twist)
+            self.twistPub.publish(twist)
 
 thorvaldID = str(sys.argv[1]) if len(sys.argv) > 1 else "001"
 rospy.init_node("roamer_" + thorvaldID)
---
+++ 2/catkin_ws/src/thorvald_roamer/scripts/see.py
@@ -0,0 +1,56 @@
+import rospy
+import sys
+from std_srvs.srv import Empty
+from std_msgs.msg import Bool
+from sensor_msgs.msg import Image
+import cv2
+from cv_bridge import CvBridge
+import numpy as np
+
+#rospy.wait_for_service('thorvald_001/spray', Empty)
+
+class Looker():
+    """Class to implement looking for green, then stopping"""
+    def __init__(self, thorvaldID):
+        self.id = thorvaldID
+        self.camSub = rospy.Subscriber("/thorvald_" + self.id + "/kinect2_camera/hd/image_color_rect", Image, self.camCallback)
+        self.camPub = rospy.Publisher("/thorvald_" + self.id + "/kinect2_camera/hd/image_color_rect_filtered", Image, queue_size=0)
+        # Publishes the filtered image on /kinect2_camera/hd/image_color_rect_filtered
+        self.stopPub = rospy.Publisher("/thorvald_" + self.id + "/STOP", Bool, queue_size=0)
+        self.bridge = CvBridge() # Bridge used for converting msgs to cv2 image class
+
+    def camCallback(self, msg):
+        # format reply
+        reply = Image()
+        reply.header = msg.header
+
+        image = self.bridge.imgmsg_to_cv2(msg, "bgr8") # Convert msg to actual cv2 image class
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV) # Convert to HSV, easier to separate green
+
+        lowerGreen = np.array([50, 0, 20]) # Lower bounds of detectable green
+        upperGreen = np.array([70, 128, 230]) # Upper bounds
+        mask = cv2.inRange(image, lowerGreen, upperGreen) # binary mask, same size as image, 1 indicates a pixel between the bounds
+        image = cv2.bitwise_and(image, image, mask=mask) # bitwise and to only keep pixels that fit in the filter
+
+        image = cv2.resize(image, (msg.width/3, msg.height/3)) # resize for sake of fitting on one screen
+        cv2.namedWindow("Image_" + self.id)
+        cv2.imshow("Image_" + self.id, cv2.cvtColor(image, cv2.COLOR_HSV2BGR))
+
+        cv2.waitKey(1)
+
+        # print(np.sum(mask)) # Print number of matched pixels for sanity check
+        if np.sum(mask) > 100000: # If enough pixels fit in the filter
+            self.stopPub.publish(True) # Publish "STOP" variable
+            # rospy.wait_for_service('thorvald_' + self.id + '/spray', Empty)
+        else:
+            self.stopPub.publish(False) # else don't stop
+        
+thorvaldID = str(sys.argv[1]) if len(sys.argv) > 1 else "001"
+rospy.init_node("looker_" + thorvaldID)
+mover = Looker(thorvaldID)
+
+print("Loaded!")
+
+rospy.spin()
+
+cv2.destroyAllWindows()
